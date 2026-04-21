354fa14e301f3d938405c3b36e229e13006c74d2
chandupriya31 <priyadavuluru@gmail.com>
2026-04-21 20:53:38 +0530

    added env

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const db = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT, 
   },
   pool: {
     min: 2,
